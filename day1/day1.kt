05e6084f0246d529e73a9f1dddbedb55e53ec642
Jack Smith <jackdsmith@google.com>
2022-12-01 17:22:08 -0800

    total overkill do it with a heap

--- day1/day1.kt
+++ day1/day1.kt
@@ -1,5 +1,6 @@
 import java.io.File
 import kotlin.io.println
+import java.util.PriorityQueue
 
 fun main(args: Array<String>) {
     val lines = File(args[0] ).readLines()
@@ -18,7 +19,20 @@ fun main(args: Array<String>) {
 
     println(totalCalorieCounts.max())
 
-    val totalCaloriesTopThree = totalCalorieCounts.sortedDescending().take(3).sum()
-
+    val totalCaloriesTopThree = totalCalorieCounts.topK(3).sum()
     println(totalCaloriesTopThree)
+}
+
+// Total overkill to do topK calc in N log K instead of N log N time
+public fun <T : Comparable<T>> Iterable<T>.topK(k: Int): Sequence<T> {
+    val iterator = iterator()
+    var heap = PriorityQueue<T>()
+    while (iterator.hasNext()) {
+        val e = iterator.next()
+        heap.add(e)
+        if(heap.size > k){
+            heap.poll()
+        }    
+    }
+    return heap.iterator().asSequence()
 }
