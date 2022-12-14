5fb99361e8e845fd661724fd60e0b43db271addf
Jack Smith <jackdsmith@google.com>
2022-12-13 23:23:22 -0800

    Part 2

--- day14/solution.kt
+++ day14/solution.kt
@@ -30,7 +30,10 @@ data class State(val maxX: Int, val minX: Int, val maxY: Int, val minY: Int, var
     fun addSand(initialPosition: Position): Boolean {
         var sandPos = initialPosition
 
-        // TODO: check if the start point is blocked
+        var charAtStartPoint = this[initialPosition.first, initialPosition.second]
+        if(charAtStartPoint != '.'){
+            return false
+        }
 
         while(isWithinBounds(sandPos)) {
             val newSandPos = nextSandPosition(sandPos)
@@ -72,10 +75,11 @@ fun main(args: Array<String>) {
     val lines = File(args[0]).readLines().map { parseLine(it)}
 
     // println(lines)
-
     val iState = makeInitialState(lines)
     println(part1(iState, Pair(500,0)))
 
+    val iStateWithFloor = makeInitialStateWithFloor(lines)
+    println(part1(iStateWithFloor, Pair(500,0)))
 }
 
 fun part1(iState: State, addPos: Position): Int {
@@ -89,9 +93,9 @@ fun part1(iState: State, addPos: Position): Int {
 // Chars are "." for empty, "#" for rock, and "O" for sand
 fun makeInitialState(lines: List<List<Position>>): State {
     // First, find the maximum extent of the positons in x and y
-    val maxX = lines.flatten().map {it.first}.max()
-    val maxY = lines.flatten().map {it.second}.max()
-    val minX = lines.flatten().map {it.first}.min()
+    var maxX = lines.flatten().map {it.first}.max()
+    var maxY = lines.flatten().map {it.second}.max()
+    var minX = lines.flatten().map {it.first}.min()
     val minY = listOf(lines.flatten().map {it.second}.min(), 0).min()
 
     var positions: MutableList<MutableList<Char>> = mutableListOf()
@@ -126,6 +130,62 @@ fun makeInitialState(lines: List<List<Position>>): State {
                 println("PROBLEM")
             }
         }
+    }
+
+    return state
+}
+
+// Chars are "." for empty, "#" for rock, and "O" for sand
+fun makeInitialStateWithFloor(lines: List<List<Position>>): State {
+    // First, find the maximum extent of the positons in x and y
+    var maxX = lines.flatten().map {it.first}.max()
+    var maxY = lines.flatten().map {it.second}.max()
+    var minX = lines.flatten().map {it.first}.min()
+    val minY = listOf(lines.flatten().map {it.second}.min(), 0).min()
+
+    val floorLevel = maxY + 2
+    maxY = floorLevel
+
+    minX = listOf(500-floorLevel-1, minX).min()
+    maxX = listOf(500+floorLevel+1, maxX).max()
+
+    var positions: MutableList<MutableList<Char>> = mutableListOf()
+
+    for(x in minX..maxX) {
+        var row = mutableListOf<Char>()
+        for(y in minY..maxY) {
+            row.add('.')
+        }
+        positions.add(row)
+    }
+
+    var state = State(maxX, minX, maxY, minY, positions)
+
+    for(line in lines) {
+        for(secondIdx in 1..line.size-1) {
+            val firstIdx = secondIdx - 1
+
+            val (firstX, firstY) = line[firstIdx]
+            val (secondX, secondY) = line[secondIdx]
+
+            if(firstX == secondX){
+                for(y in reversibleRange(firstY, secondY)) {
+                    state[firstX, y] = '#'
+                }
+            }
+            else if(firstY == secondY) {
+                for(x in reversibleRange(firstX, secondX)) {
+                    state[x, firstY] = '#'
+                }
+            } else {
+                println("PROBLEM")
+            }
+        }
+    }
+
+    // Draw the floor
+    for(x in minX..maxX){
+        state[x, floorLevel] = '#'
     }
 
     return state
