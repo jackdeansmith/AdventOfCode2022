1838363986a8c010fc2bc6071e91ca4f0475fb9f
Jack Smith <jackdsmith@google.com>
2022-12-20 16:33:49 -0800

    Day20 part 2

--- day20/solution.kt
+++ day20/solution.kt
@@ -3,10 +3,10 @@ import kotlin.io.println
 import kotlin.math.abs
 
 fun main(args: Array<String>) {
-    val numbers = File(args[0]).readLines().map{it.toInt()}
+    val numbers = File(args[0]).readLines().map{it.toLong()}
 
     // Verified, the list contains duplicates
-    // var set: MutableSet<Int> = mutableSetOf()
+    // var set: MutableSet<Long> = mutableSetOf()
     // for(x in numbers) {
     //     if(set.contains(x)){
     //         println("duplicate: $x")
@@ -19,9 +19,26 @@ fun main(args: Array<String>) {
     val indexedNumbers = numbers.withIndex().map {(idx, num) -> Pair(idx, num)}
 
     // Simple enough now, I need to find each number by index and move it
-    var result = indexedNumbers.toMutableList()
-    // println(result)
+    var result1 = mix(indexedNumbers).map{it.second}
+    val pt1 = answerFromPlaintext(result1)
+    println("Part1: $pt1")
 
+    // Part 2, multiply all numbers by decryption key
+    val key = 811589153
+    val newIndexedNumbers = indexedNumbers.map { Pair(it.first, key * it.second)}
+    // println(newIndexedNumbers)
+
+    var result2 = newIndexedNumbers.toMutableList()
+    repeat(10) {
+        result2 = mix(result2).toMutableList()
+    }
+    var result2Nums = result2.map{it.second}
+    val pt2 = answerFromPlaintext(result2Nums)
+    println("Part2: $pt2")
+}
+
+fun mix(items: List<Pair<Int, Long>>): List<Pair<Int, Long>> {
+    var result = items.toMutableList()
     for(origIndex in 0..result.size-1) {
 
         var foundAtIndex = 0
@@ -39,19 +56,12 @@ fun main(args: Array<String>) {
             newIndex = result.size
         }
 
-        // println("moving: $origIndex, rawNewIndex: ${(foundAtIndex + toBeMoved.second)}")
-        // println("newIndex: $newIndex, tobemoved: $toBeMoved")
-
         result.add(newIndex, toBeMoved)
-        // println(result.map {it.second})
-
     }
-
-    val pt1 = answerFromPlaintext(result.map{it.second})
-    println("Part1: $pt1")
+    return result
 }
 
-fun answerFromPlaintext(plaintext: List<Int>): Int {
+fun answerFromPlaintext(plaintext: List<Long>): Long {
     val indexOfZero = plaintext.indexOf(0)
     val cord1 = plaintext[(indexOfZero + 1000).mod(plaintext.size)]
     val cord2 = plaintext[(indexOfZero + 2000).mod(plaintext.size)]
